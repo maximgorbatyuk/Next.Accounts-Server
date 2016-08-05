49581333f87a2c7d6f0d254fcc03f55dd1f18aa7
WorkPC <maximgorbatyuk191093@gmail.com>
2016-08-05 07:40:57 +0600

    Client added

---
+++ Next.Accounts Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Next.Accounts Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Next.Accounts Client")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("44e7e9ac-e263-4097-a3d8-40236110b501")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Next.Accounts Server/Controllers/DefaultLogger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Next.Accounts_Server.Controllers
+{
+    public class DefaultLogger : ILogger
+    {
+
+        public string ErrorLogName { get; set; } = "errors.txt";
+
+        public string EventsLogName { get; set; } = "database_events.txt";
+
+        public string UsementLogName { get; set; } = "usement.txt";
+
+        private string _logPath;
+
+        public DefaultLogger()
+        {
+            _logPath = $"{Environment.CurrentDirectory}\\Logs";
+        }
+
+        public void Log(string message)
+        {
+            var filename = $"{_logPath}\\{EventsLogName}";
+            WriteToFile(filename, message);
+        }
+
+        public void LogError(string error)
+        {
+            var filename = $"{_logPath}\\{ErrorLogName}";
+            WriteToFile(filename, error);
+        }
+
+        public void LogAccountUsement(string message)
+        {
+            UsementLogName = $"usement-{DateTime.Now.ToString("dd-MM-yyyy")}.txt";
+            var filename = $"{_logPath}\\{UsementLogName}";
+            WriteToFile(filename, message);
+        }
+
+        public async void WriteToFile(string filename, string write)
+        {
+            //if (!File.Exists(filename)) File.Create(filename);
+            using (var stream = File.AppendText(filename))
+            {
+                var text = $"[{DateTime.Now}] {write}";
+                await stream.WriteLineAsync(text);
+            }
+        }
+    }
+}
